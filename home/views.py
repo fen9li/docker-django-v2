ae509b70b5c73e37bf094284b528229a8eb77635
Feng Li <lifcn@yahoo.com>
2019-07-14 19:59:45 +1000

    changelog006 add home movies view

--- home/views.py
+++ home/views.py
@@ -1,8 +1,8 @@
-from django.shortcuts import render
+# from django.shortcuts import render
 
 # Create your views here.
-from django.http import HttpResponse
+from django.views.generic import ListView
+from home.models import Movie
 
-
-def index(request):
-    return HttpResponse("Hello, world. You're at home.")
+class MovieList(ListView):
+    model = Movie
